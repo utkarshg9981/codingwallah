810bb07be3345b3f8099d2459b12bd841fddbc65
utkarshg9981 <utkarshg9981@gmail.com>
2025-09-22 15:21:37 +0530

    second

--- src/main/java/com/codingwallah/codingwallah/CodingwallahApplication.java
+++ src/main/java/com/codingwallah/codingwallah/CodingwallahApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodingwallahApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+	
 		SpringApplication.run(CodingwallahApplication.class, args);
 	}
 
